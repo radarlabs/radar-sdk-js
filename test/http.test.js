1a82d955d7ee1d51d40b0c6122b0d2d3aed344a2
Jay Sani <jay@radar.io>
2019-12-23 14:33:34 -0500

    http GET tests

--- test/http.test.js
+++ test/http.test.js
@@ -1,11 +1,12 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
 
+import SDK_VESRION from '../src/version';
 import STATUS from '../src/status_codes';
 import * as Http from '../src/http';
 
 describe('http', () => {
-  describe('request', () => {
+  describe('request PUT', () => {
 
     let request;
     let successCallback;
@@ -86,6 +87,58 @@ describe('http', () => {
         expect(errorCallback).to.have.been.calledWith(STATUS.ERROR_NETWORK);
         expect(successCallback).to.not.have.been.called;
       });
+    });
+  });
+
+  describe('request GET', () => {
+
+    let request;
+    let successCallback;
+    let errorCallback;
+
+    let data;
+    let getResponse;
+
+    beforeEach(() => {
+      global.XMLHttpRequest = sinon.useFakeXMLHttpRequest();
+
+      global.XMLHttpRequest.onCreate = (xhrRequest) => {
+        request = xhrRequest;
+      };
+
+      successCallback = sinon.spy();
+      errorCallback = sinon.spy();
+
+      data = { query: '20 Jay Street' };
+      getResponse = '{ meta: { code: 200 }, addresses: [{ latitude: 40.7039, longitude: -73.9867 }] }';
+
+      Http.request('GET', 'https://api.radar.io/v1/geocode/forward', data, { 'Authorization': 'publishable-key' }, successCallback, errorCallback);
+    });
+
+    afterEach(() => {
+      global.XMLHttpRequest.restore();
+    });
+
+    it('should always include Device-Type and SDK-Version headers', () => {
+      expect(request).to.not.be.null;
+      request.respond(200, {}, getResponse);
+
+      expect(successCallback).to.be.calledWith(getResponse);
+      expect(errorCallback).to.not.be.called;
+
+      expect(request.requestHeaders['X-Radar-Device-Type'], 'Web');
+      expect(request.requestHeaders['X-Radar-SDK-Version'], SDK_VESRION);
+    });
+
+    it('should inject GET parameters into the url querystring', () => {
+      expect(request).to.not.be.null;
+      request.respond(200, {}, getResponse);
+
+      expect(successCallback).to.be.calledWith(getResponse);
+      expect(errorCallback).to.not.be.called;
+
+      const urlencodedData = encodeURIComponent(`query=${data.query}`);
+      expect(request.url).to.contain(`?${urlencodedData}`);
     });
   });
 });
