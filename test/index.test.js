641dd15cab0a5979fa28970c7e0af3f9dad7178d
Jay Sani <jay@radar.io>
2020-01-03 14:39:59 -0500

    search and geocoding tests

--- test/index.test.js
+++ test/index.test.js
@@ -9,6 +9,7 @@ import * as Cookie from '../src/cookie';
 import * as Device from '../src/device';
 import * as Http from '../src/http';
 import Radar from '../src/index';
+
 import PLACES_PROVIDER from '../src/places_providers';
 import SDK_VERSION from '../src/version';
 import STATUS from '../src/status_codes';
@@ -307,6 +308,407 @@ describe('Radar', () => {
         Radar.trackOnce(callback);
 
         expect(callback).to.have.been.calledWith('ajax error');
+      });
+    });
+  });
+
+  describe('search', () => {
+    const publishableKey = 'mock-publishable-key';
+
+    const latitude = 40.7041895;
+    const longitude = -73.9867797;
+    const accuracy = 1;
+    const position = {
+      coords: { accuracy, latitude, longitude }
+    };
+
+    const mockRadius = 100;
+    const mockChains = ['dunkin', 'sbucks'];
+    const mockLimit = 50;
+
+    context('searchPlacesWithLocation', () => {
+      it('should return a publishable key error if not set', () => {
+        getCookieStub.withArgs(Cookie.PUBLISHABLE_KEY).returns(null);
+
+        const searchCallback = sinon.spy();
+        Radar.searchPlacesWithLocation(latitude, longitude, mockRadius, mockChains, [], [], mockLimit, searchCallback);
+
+        expect(searchCallback).to.be.calledWith(STATUS.ERROR_PUBLISHABLE_KEY);
+      });
+
+      it('should throw a server error if invalid JSON is returned in the response', () => {
+        getCookieStub.withArgs(Cookie.PUBLISHABLE_KEY).returns(publishableKey);
+
+        const jsonErrorResponse = '"invalid_json": true}';
+        const httpRequestSpy = sinon.spy((method, url, body, headers, onSuccess, onError) => {
+          onSuccess(jsonErrorResponse);
+        });
+        sinon.stub(Http, 'request').callsFake(httpRequestSpy);
+
+        const searchCallback = sinon.spy();
+        Radar.searchPlacesWithLocation(latitude, longitude, mockRadius, mockChains, [], [], mockLimit, searchCallback);
+
+        expect(searchCallback).to.be.calledWith(STATUS.ERROR_SERVER);
+
+        Http.request.restore();
+      });
+
+      it('should return the error from the http request', () => {
+        getCookieStub.withArgs(Cookie.PUBLISHABLE_KEY).returns(publishableKey);
+
+        const httpRequestSpy = sinon.spy((method, url, body, headers, onSuccess, onError) => {
+          onError('http error');
+        });
+        sinon.stub(Http, 'request').callsFake(httpRequestSpy);
+
+        const searchCallback = sinon.spy();
+        Radar.searchPlacesWithLocation(latitude, longitude, mockRadius, mockChains, [], [], mockLimit, searchCallback);
+
+        expect(searchCallback).to.be.calledWith('http error');
+
+        Http.request.restore();
+      });
+
+      it('should succeed', () => {
+        getCookieStub.withArgs(Cookie.PUBLISHABLE_KEY).returns(publishableKey);
+
+        const jsonSuccessResponse = '{"places":"matching-places"}';
+        const httpRequestSpy = sinon.spy((method, url, body, headers, onSuccess, onError) => {
+          onSuccess(jsonSuccessResponse);
+        });
+        sinon.stub(Http, 'request').callsFake(httpRequestSpy);
+
+        const searchCallback = sinon.spy();
+        Radar.searchPlacesWithLocation(latitude, longitude, mockRadius, mockChains, [], [], mockLimit, searchCallback);
+
+        const [method, url, body, headers] = httpRequestSpy.getCall(0).args;
+        expect(method).to.equal('GET');
+        expect(url).to.equal('https://api.radar.io/v1/search/places');
+        expect(headers).to.deep.equal({
+          Authorization: publishableKey
+        });
+        expect(body).to.deep.equal({
+          latitude,
+          longitude,
+          radius: mockRadius,
+          chains: mockChains.join(','),
+          categories: '',
+          groups: '',
+          limit: mockLimit
+        });
+
+        expect(searchCallback).to.be.calledWith(STATUS.SUCCESS, 'matching-places');
+
+        Http.request.restore();
+      });
+    });
+
+    context('searchGeofencesWithLocation', () => {
+      it('should return a publishable key error if not set', () => {
+        getCookieStub.withArgs(Cookie.PUBLISHABLE_KEY).returns(null);
+
+        const searchCallback = sinon.spy();
+        Radar.searchGeofencesWithLocation(latitude, longitude, mockRadius, [], mockLimit, searchCallback);
+
+        expect(searchCallback).to.be.calledWith(STATUS.ERROR_PUBLISHABLE_KEY);
+      });
+
+      it('should throw a server error if invalid JSON is returned in the response', () => {
+        getCookieStub.withArgs(Cookie.PUBLISHABLE_KEY).returns(publishableKey);
+
+        const jsonErrorResponse = '"invalid_json": true}';
+        const httpRequestSpy = sinon.spy((method, url, body, headers, onSuccess, onError) => {
+          onSuccess(jsonErrorResponse);
+        });
+        sinon.stub(Http, 'request').callsFake(httpRequestSpy);
+
+        const searchCallback = sinon.spy();
+        Radar.searchGeofencesWithLocation(latitude, longitude, mockRadius, [], mockLimit, searchCallback);
+
+        expect(searchCallback).to.be.calledWith(STATUS.ERROR_SERVER);
+
+        Http.request.restore();
+      });
+
+      it('should return the error form the http request', () => {
+        getCookieStub.withArgs(Cookie.PUBLISHABLE_KEY).returns(publishableKey);
+
+        const httpRequestSpy = sinon.spy((method, url, body, headers, onSuccess, onError) => {
+          onError('http error');
+        });
+        sinon.stub(Http, 'request').callsFake(httpRequestSpy);
+
+        const searchCallback = sinon.spy();
+        Radar.searchGeofencesWithLocation(latitude, longitude, mockRadius, [], mockLimit, searchCallback);
+
+        expect(searchCallback).to.be.calledWith('http error');
+
+        Http.request.restore();
+      });
+
+      it('should succeed', () => {
+        getCookieStub.withArgs(Cookie.PUBLISHABLE_KEY).returns(publishableKey);
+
+        const jsonSuccessResponse = '{"geofences":"matching-geofences"}';
+        const httpRequestSpy = sinon.spy((method, url, body, headers, onSuccess, onError) => {
+          onSuccess(jsonSuccessResponse);
+        });
+        sinon.stub(Http, 'request').callsFake(httpRequestSpy);
+
+        const searchCallback = sinon.spy();
+        Radar.searchGeofencesWithLocation(latitude, longitude, mockRadius, [], mockLimit, searchCallback);
+
+        const [method, url, body, headers] = httpRequestSpy.getCall(0).args;
+        expect(method).to.equal('GET');
+        expect(url).to.equal('https://api.radar.io/v1/search/geofences');
+        expect(headers).to.deep.equal({
+          Authorization: publishableKey
+        });
+        expect(body).to.deep.equal({
+          latitude,
+          longitude,
+          radius: mockRadius,
+          tags: '',
+          limit: mockLimit
+        });
+
+        expect(searchCallback).to.be.calledWith(STATUS.SUCCESS, 'matching-geofences');
+
+        Http.request.restore();
+      });
+    });
+  });
+
+  describe('geocoding', () => {
+    const publishableKey = 'mock-publishable-key';
+
+    const latitude = 40.7041895;
+    const longitude = -73.9867797;
+
+    const ip = '192.0.2.28';
+
+    const mockQuery = '20 Jay Street';
+
+    context('geocode', () => {
+      it('should return a publishable key error if not set', () => {
+        getCookieStub.withArgs(Cookie.PUBLISHABLE_KEY).returns(null);
+
+        const geocodeCallback = sinon.spy();
+        Radar.geocode(mockQuery, geocodeCallback);
+
+        expect(geocodeCallback).to.be.calledWith(STATUS.ERROR_PUBLISHABLE_KEY);
+      });
+
+      it('should throw a server error if invalid JSON is returned in the response', () => {
+        getCookieStub.withArgs(Cookie.PUBLISHABLE_KEY).returns(publishableKey);
+
+        const jsonErrorResponse = '"invalid_json": true}';
+        const httpRequestSpy = sinon.spy((method, url, body, headers, onSuccess, onError) => {
+          onSuccess(jsonErrorResponse);
+        });
+        sinon.stub(Http, 'request').callsFake(httpRequestSpy);
+
+        const geocodeCallback = sinon.spy();
+        Radar.geocode(mockQuery, geocodeCallback);
+
+        expect(geocodeCallback).to.be.calledWith(STATUS.ERROR_SERVER);
+
+        Http.request.restore();
+      });
+
+      it('should return the error from the http request', () => {
+        getCookieStub.withArgs(Cookie.PUBLISHABLE_KEY).returns(publishableKey);
+
+        const httpRequestSpy = sinon.spy((method, url, body, headers, onSuccess, onError) => {
+          onError('http error');
+        });
+        sinon.stub(Http, 'request').callsFake(httpRequestSpy);
+
+        const geocodeCallback = sinon.spy();
+        Radar.geocode(mockQuery, geocodeCallback);
+
+        expect(geocodeCallback).to.be.calledWith('http error');
+
+        Http.request.restore();
+      });
+
+      it('should succeed', () => {
+        getCookieStub.withArgs(Cookie.PUBLISHABLE_KEY).returns(publishableKey);
+
+        const jsonSuccessResponse = '{"addresses":["matching-addresses"]}';
+        const httpRequestSpy = sinon.spy((method, url, body, headers, onSuccess, onError) => {
+          onSuccess(jsonSuccessResponse);
+        });
+        sinon.stub(Http, 'request').callsFake(httpRequestSpy);
+
+        const geocodeCallback = sinon.spy();
+        Radar.geocode(mockQuery, geocodeCallback);
+
+        const [method, url, body, headers] = httpRequestSpy.getCall(0).args;
+        expect(method).to.equal('GET');
+        expect(url).to.equal('https://api.radar.io/v1/geocode/forward');
+        expect(headers).to.deep.equal({
+          Authorization: publishableKey
+        });
+        expect(body).to.deep.equal({
+          query: mockQuery
+        });
+
+        expect(geocodeCallback).to.be.calledWith(STATUS.SUCCESS, ['matching-addresses']);
+
+        Http.request.restore();
+      });
+    });
+
+    context('reverseGeocodeLocation', () => {
+      it('should return a publishable key error if not set', () => {
+        getCookieStub.withArgs(Cookie.PUBLISHABLE_KEY).returns(null);
+
+        const geocodeCallback = sinon.spy();
+        Radar.reverseGeocodeLocation(latitude, longitude, geocodeCallback);
+
+        expect(geocodeCallback).to.be.calledWith(STATUS.ERROR_PUBLISHABLE_KEY);
+      });
+
+      it('should throw a server error if invalid JSON is returned in the response', () => {
+        getCookieStub.withArgs(Cookie.PUBLISHABLE_KEY).returns(publishableKey);
+
+        const jsonErrorResponse = '"invalid_json": true}';
+        const httpRequestSpy = sinon.spy((method, url, body, headers, onSuccess, onError) => {
+          onSuccess(jsonErrorResponse);
+        });
+        sinon.stub(Http, 'request').callsFake(httpRequestSpy);
+
+        const geocodeCallback = sinon.spy();
+        Radar.reverseGeocodeLocation(latitude, longitude, geocodeCallback);
+
+        expect(geocodeCallback).to.be.calledWith(STATUS.ERROR_SERVER);
+
+        Http.request.restore();
+      });
+
+      it('should return the error from the http request', () => {
+        getCookieStub.withArgs(Cookie.PUBLISHABLE_KEY).returns(publishableKey);
+
+        const httpRequestSpy = sinon.spy((method, url, body, headers, onSuccess, onError) => {
+          onError('http error');
+        });
+        sinon.stub(Http, 'request').callsFake(httpRequestSpy);
+
+        const geocodeCallback = sinon.spy();
+        Radar.reverseGeocodeLocation(latitude, longitude, geocodeCallback);
+
+        expect(geocodeCallback).to.be.calledWith('http error');
+
+        Http.request.restore();
+      });
+
+      it('should succeed', () => {
+        getCookieStub.withArgs(Cookie.PUBLISHABLE_KEY).returns(publishableKey);
+
+        const jsonSuccessResponse = '{"addresses":["matching-addresses"]}';
+        const httpRequestSpy = sinon.spy((method, url, body, headers, onSuccess, onError) => {
+          onSuccess(jsonSuccessResponse);
+        });
+        sinon.stub(Http, 'request').callsFake(httpRequestSpy);
+
+        const geocodeCallback = sinon.spy();
+        Radar.reverseGeocodeLocation(latitude, longitude, geocodeCallback);
+
+        const [method, url, body, headers] = httpRequestSpy.getCall(0).args;
+        expect(method).to.equal('GET');
+        expect(url).to.equal('https://api.radar.io/v1/geocode/reverse');
+        expect(headers).to.deep.equal({
+          Authorization: publishableKey
+        });
+        expect(body).to.deep.equal({
+          latitude,
+          longitude
+        });
+
+        expect(geocodeCallback).to.be.calledWith(STATUS.SUCCESS, ['matching-addresses']);
+
+        Http.request.restore();
+      });
+    });
+
+    context('geocodeDeviceIP', () => {
+      it('should succeed - calling geocodeIP with undefined', () => {
+        const geocodeIPSpy = sinon.spy((ip, callback) => {
+          callback(STATUS.SUCCESS, { country: 'USA'} );
+        });
+        sinon.stub(Radar, 'geocodeIP').callsFake(geocodeIPSpy);
+
+        const geocodeCallback = sinon.spy();
+        Radar.geocodeDeviceIP(geocodeCallback);
+
+        const [ip] = geocodeIPSpy.getCall(0).args;
+        expect(ip).to.be.undefined;
+
+        expect(geocodeCallback).to.be.calledWith(STATUS.SUCCESS, { country: 'USA' });
+
+        Radar.geocodeIP.restore();
+      });
+    });
+
+    context('geocodeIP', () => {
+      it('should return a publishable key error if not set', () => {
+        getCookieStub.withArgs(Cookie.PUBLISHABLE_KEY).returns(null);
+
+        const geocodeCallback = sinon.spy();
+        Radar.geocodeIP(ip, geocodeCallback);
+
+        expect(geocodeCallback).to.be.calledWith(STATUS.ERROR_PUBLISHABLE_KEY);
+      });
+
+      it('should throw a server error if invalid JSON is returned in the response', () => {
+        getCookieStub.withArgs(Cookie.PUBLISHABLE_KEY).returns(publishableKey);
+
+        const jsonErrorResponse = '"invalid_json": true}';
+        const httpRequestSpy = sinon.spy((method, url, body, headers, onSuccess, onError) => {
+          onSuccess(jsonErrorResponse);
+        });
+        sinon.stub(Http, 'request').callsFake(httpRequestSpy);
+
+        const geocodeCallback = sinon.spy();
+        Radar.geocodeIP(ip, geocodeCallback);
+
+        expect(geocodeCallback).to.be.calledWith(STATUS.ERROR_SERVER);
+
+        Http.request.restore();
+      });
+
+      it('should return the error from the http request', () => {
+        getCookieStub.withArgs(Cookie.PUBLISHABLE_KEY).returns(publishableKey);
+
+        const httpRequestSpy = sinon.spy((method, url, body, headers, onSuccess, onError) => {
+          onError('http error');
+        });
+        sinon.stub(Http, 'request').callsFake(httpRequestSpy);
+
+        const geocodeCallback = sinon.spy();
+        Radar.geocodeIP(ip, geocodeCallback);
+
+        expect(geocodeCallback).to.be.calledWith('http error');
+
+        Http.request.restore();
+      });
+
+      it('should succeed', () =>{
+        getCookieStub.withArgs(Cookie.PUBLISHABLE_KEY).returns(publishableKey);
+
+        const jsonSuccessResponse = '{"country":"matching-country"}';
+        const httpRequestSpy = sinon.spy((method, url, body, headers, onSuccess, onError) => {
+          onSuccess(jsonSuccessResponse);
+        });
+        sinon.stub(Http, 'request').callsFake(httpRequestSpy);
+
+        const geocodeCallback = sinon.spy();
+        Radar.geocodeIP(ip, geocodeCallback);
+
+        expect(geocodeCallback).to.be.calledWith(STATUS.SUCCESS, 'matching-country');
+
+        Http.request.restore();
       });
     });
   });
