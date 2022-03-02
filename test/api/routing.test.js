a2800bc6e61ebac2e80245c33e927379219072eb
Sam Fader <samfader@users.noreply.github.com>
2022-03-02 11:46:54 -0800

    move matrix tests in routing test file

--- test/api/routing.test.js
+++ test/api/routing.test.js
@@ -12,6 +12,7 @@ import Navigator from '../../src/navigator';
 import STATUS from '../../src/status';
 
 import Routing from '../../src/api/routing';
+import Matrix from '../../src/api/matrix';
 
 describe('Routing', () => {
   let httpStub;
@@ -74,6 +75,70 @@ describe('Routing', () => {
           httpStub.resolves(routingResponse);
 
           return Routing.getDistanceToDestination({ destination, modes, units })
+            .then((response) => {
+              expect(response).to.equal(routingResponse);
+            });
+        });
+      });
+    });
+  });
+});
+
+describe('Matrix', () => {
+  let httpStub;
+  let navigatorStub;
+
+  const origin = '40.73237,-73.94884';
+
+  const destination = '40.70390,-73.98690|40.73237,-73.94884';
+
+  const mode = 'car';
+  const units = 'imperial';
+
+  const routingResponse = { meta: {}, routes: {} };
+
+  beforeEach(() => {
+    navigatorStub = sinon.stub(Navigator, 'getCurrentPosition');
+    httpStub = sinon.stub(Http, 'request');
+  });
+
+  afterEach(() => {
+    Navigator.getCurrentPosition.restore();
+    Http.request.restore();
+  });
+
+  context('getMatrixDistances', () => {
+    describe('location permissions denied', () => {
+      it('should throw a navigation error', () => {
+        navigatorStub.rejects(STATUS.ERROR_PERMISSIONS);
+
+        return Matrix.getMatrixDistances()
+          .catch((err) => {
+            expect(err.toString()).to.eq(STATUS.ERROR_PERMISSIONS);
+            expect(httpStub).to.have.callCount(0);
+          });
+      });
+    });
+
+    describe('location permissions approved', () => {
+      describe('no args given', () => {
+        it('should return a routing response', () => {
+          navigatorStub.resolves(origin);
+          httpStub.resolves(routingResponse);
+
+          return Matrix.getMatrixDistances()
+            .then((response) => {
+              expect(response).to.equal(routingResponse);
+            });
+        });
+      });
+
+      describe('all args given', () => {
+        it('should return a routing response', () => {
+          navigatorStub.resolves(origin);
+          httpStub.resolves(routingResponse);
+
+          return Matrix.getMatrixDistances({ destination, mode, units })
             .then((response) => {
               expect(response).to.equal(routingResponse);
             });
