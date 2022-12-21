0a26cf83ec7ffbd42326d6517850a7e715e95f51
Brett Guenther <brett@radar.io>
2022-12-21 12:06:26 -0700

    custom events api (#80)
    * custom events api
    
    * fix tests and revert trip response
    
    * fix indents
    
    * add created at
    
    * add created at to param

---
+++ test/api/events.test.js
@@ -0,0 +1,40 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+
+chai.use(sinonChai);
+const { expect } = chai;
+
+import Http from '../../src/http';
+import STATUS from '../../src/status';
+
+import Events from '../../src/api/events';
+
+describe('Events', () => {
+    let httpStub;
+
+    const eventResponse = { meta: {}, event: {} };
+
+    beforeEach(() => {
+        httpStub = sinon.stub(Http, 'request');
+    });
+
+    const customEventType = 'app_open';
+    const customEventMetadata = {'source':'organic'};
+    const customEventData = {customEventType, customEventMetadata}
+
+    afterEach(() => {
+        Http.request.restore();
+    });
+
+    context('events', () => {
+        it('should return an event', () => {
+          httpStub.resolves(eventResponse);
+    
+          return Events.sendEvent({ customEventData })
+            .then((response) => {
+              expect(response).to.equal(eventResponse);
+            });
+        });
+    });
+});
