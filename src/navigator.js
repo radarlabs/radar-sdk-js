2da1237f357bb89a6b1aacbdee9b27c73130e083
Boon Boonsiri <35120291+boonboonsiri@users.noreply.github.com>
2023-02-03 16:05:29 -0500

    RAD-5740: cache-recent-locations-in-websdk-with-variable-ttl (#86)
    * Basic layout code to cache tiem
    
    * Added functionality to store values
    
    * Added functionality to store time to live in options
    
    * formatting
    
    * Unix time in js is in fact in miliseconds plus fixing type casting
    
    * set variable name
    
    * fixes
    
    * moved changes to be for all navigatin and not just track.trackOnce
    
    * formatting
    
    * Craig changes, store things differently, bug fixes
    
    * Craig review 2, renamed vars
    
    * Cory review, formatting, etc
    
    * lint
    
    * v3.5.1-beta
    
    * bump to v3.5.1
    
    ---------
    
    Co-authored-by: cory pisano <cory.pisano@gmail.com>

--- src/navigator.js
+++ src/navigator.js
@@ -1,4 +1,5 @@
 import STATUS from './status';
+import Storage from './storage';
 
 class Navigator {
   static getCurrentPosition() {
@@ -6,6 +7,23 @@ class Navigator {
 
       if (!navigator || !navigator.geolocation) {
         return reject(STATUS.ERROR_LOCATION);
+      }
+
+      const cacheLocationMinutes = parseFloat(Storage.getItem(Storage.CACHE_LOCATION_MINUTES));
+      if (cacheLocationMinutes) {
+        try {
+          const lastLocation = JSON.parse(Storage.getItem(Storage.LAST_LOCATION));
+          if (lastLocation) {
+            const { latitude, longitude, accuracy } = lastLocation;
+            if (Date.now() < parseInt(lastLocation.expiresAt)) {            // check expiration stuff goes here
+              if (latitude && longitude && accuracy) {
+                return resolve({ latitude, longitude, accuracy });
+              }
+            }
+          }
+        } catch (e) {
+          console.warn('Radar SDK: could not load cached location.');
+        }
       }
 
       navigator.geolocation.getCurrentPosition(
@@ -16,6 +34,14 @@ class Navigator {
           }
 
           const { latitude, longitude, accuracy } = position.coords;
+
+          if (cacheLocationMinutes) {
+            const updatedAt = Date.now();
+            const expiresAt = updatedAt + (cacheLocationMinutes * 60 * 1000); // convert to ms
+
+            const lastLocation = { latitude, longitude, accuracy, updatedAt, expiresAt };
+            Storage.setItem(Storage.LAST_LOCATION, JSON.stringify(lastLocation));
+          }
 
           return resolve({ latitude, longitude, accuracy });
         },
