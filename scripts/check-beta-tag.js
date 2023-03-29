76674e91bd98832c7e862f4ff46e5516a5fe9fd7
Tim Julien <tim@radar.io>
2023-03-29 17:10:25 -0400

    Github Action for releases (#94)
    * Github Action for releases
    
    * Update radar-release-actions.yml
    
    * add steps for building and testing
    
    * prepend 'v'
    
    * add version checks
    
    * only run check-tags on prepublish
    
    * log tag from script
    
    * dont prepend "v" as its included in the tag
    
    * wrap tests in a context
    
    * separate workflow for pre-release
    
    * use --force when installing deps
    
    * use ref_name to get just the tag name
    
    * bump version
    
    * bump version, and add script to auto incremenet
    
    * set version to non-beta
    
    ---------
    
    Co-authored-by: Craig Kochis <cjkochis@gmail.com>

---
+++ scripts/check-beta-tag.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const packageJSON = require('../package.json');
+const packageLockJSON = require('../package-lock.json');
+
+let tagVersion = process.env.GITHUB_REF_NAME || '';
+console.log('Checking tag:', tagVersion);
+
+if (!tagVersion.startsWith('v')) {
+  console.error('Tag must start with "v"');
+  process.exit(1);
+}
+if (!tagVersion.includes('-beta')) {
+  console.error('Prelease version should contiain a "-beta" suffix');
+  process.exit(1);
+}
+tagVersion = tagVersion.slice(1); // remove "v"
+
+const fileVersion = fs.readFileSync('./src/version.js')
+  .toString()
+  .replace('export default', '')
+  .replaceAll('\'', '')
+  .replace(';', '')
+  .trim();
+
+if (tagVersion !== fileVersion) {
+  console.error('VERSION MISMATCH - version does not match ./src/version.js');
+  process.exit(1);
+}
+if (tagVersion !== packageJSON.version) {
+  console.error('VERSION MISMATCH - version does not match package.json.');
+  process.exit(1);
+}
+if (tagVersion !== packageLockJSON.version) {
+  console.error('VERSION MISMATCH - version does not match package-lock.json.');
+  process.exit(1);
+}
+console.log('Tag OK.', tagVersion);
