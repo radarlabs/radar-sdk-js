c882a69986a9922bed16c92410bc7395ae6e7e10
Nick Patrick <nicholasjohnpatrick@gmail.com>
2024-01-20 13:19:53 -0500

    refactor

--- src/api/track.ts
+++ src/api/track.ts
@@ -7,37 +7,9 @@ import Navigator from '../navigator';
 import Session from '../session';
 import Storage from '../storage';
 import TripsAPI from './trips';
+import { signJWT } from '../util/jwt'
 
 import type { RadarTrackParams, RadarTrackResponse } from '../types';
-
-const base64Encode = (str: string): string => 
-  btoa(str).replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
-
-const signJWT = async (payload: object, key: string): Promise<string> => {
-  const encoder = new TextEncoder();
-
-  const encodedHeader = base64Encode(JSON.stringify({
-    alg: 'HS256',
-    typ: 'JWT',
-  }));
-  const encodedPayload = base64Encode(JSON.stringify(payload));
-
-  const keyData = encoder.encode(key);
-  const messageData = encoder.encode(`${encodedHeader}.${encodedPayload}`);
-
-  const cryptoKey = await crypto.subtle.importKey(
-    'raw',
-    keyData,
-    { name: 'HMAC', hash: { name: 'SHA-256' } },
-    false,
-    ['sign']
-  );
-
-  const signatureArrayBuffer = await crypto.subtle.sign('HMAC', cryptoKey, messageData);
-  const signature = base64Encode(String.fromCharCode(...Array.from(new Uint8Array(signatureArrayBuffer))));
-
-  return `${encodedHeader}.${encodedPayload}.${signature}`;
-};
 
 class TrackAPI {
   static async trackOnce(params: RadarTrackParams) {
