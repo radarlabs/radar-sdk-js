429c243d673ef62669c92da39195f3573f800617
Luke Zhang <luke.zhang@radar.com>
2026-02-11 17:43:04 -0500

    expose RadarError class publically

--- src/iife-entry.ts
+++ src/iife-entry.ts
@@ -1,1 +1,8 @@
-export { default } from './index';
+import RadarBase, {RadarError} from "."
+
+// Any other non-type exports that we need to be able to access from other CDNs go here
+class Radar extends RadarBase {
+    public static RadarError = RadarError;
+}
+
+export default Radar
